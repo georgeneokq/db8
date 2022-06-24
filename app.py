a8494e35810601cb0cb91631c1c0a3c3f46aaf5c
George <george.neokq@gmail.com>
2022-06-24 13:55:27 +0800

    Add route for POST /login

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def db8():
   return render_template('db8-login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+  return render_template('db8-login.html')
